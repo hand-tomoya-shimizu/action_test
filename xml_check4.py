e444b4cb6cb3e440004469589c416a39fab1900d
shimizu_t <shimizu_t@hand.co.jp>
2023-03-24 21:33:23 +0900

    update

--- xml_check4.py
+++ xml_check4.py
@@ -7,16 +7,23 @@ print(f"@ pull_request_url: {pull_request_url}")
 
 # APIリクエストを送信して、変更内容を取得する
 response = requests.get(pull_request_url)
-print(f"@ response: {response}")
+print(f"@ response1: {response}")
 
 pull_request_data = response.json()
-print(f"@ files: {pull_request_data['files']}")
 
-# プルリクエストの変更内容にXMLファイルが含まれているか確認する
-for file in pull_request_data['files']:
-    if file['filename'].endswith('.xml'):
-        print("プルリクエストでXMLファイルが変更されました")
-        exit(1)
+# 変更されたファイルの内容を取得して、XMLファイルが含まれているか確認する
+diff_url = pull_request_data['diff_url']
+print(f"@ diff_url: {diff_url}")
+
+diff_response = requests.get(diff_url)
+print(f"@ response2: {response}")
+
+diff_text = diff_response.text
+print(f"@ diff_text: {diff_text}")
+
+if '.xml' in diff_text:
+    print("プルリクエストでXMLファイルが変更されました")
+    exit(1)
 
 print("プルリクエストでXMLファイルは変更されていません")
 exit(0)
