c9db2c0e43b3ca292c146870bce499f717beb906
shimizu_t <shimizu_t@hand.co.jp>
2023-03-24 10:46:56 +0900

    update

--- xml_check.js
+++ xml_check.js
@@ -5,22 +5,22 @@ async function run() {
   try {
     const pull_request_url = process.env.GITHUB_PULL_REQUEST_URL;
     const files_url = `${pull_request_url}/files`;
-    console.log(`files_url: ${files_url}`);
+    console.log(`@ files_url: ${files_url}`);
 
     const response = await fetch(files_url);
-    console.log(`response: ${response}`);
+    console.log(`@ response: ${response}`);
 
     const data = await response.json();
-    console.log(`data: ${data}`);
+    console.log(`@ data: ${data}`);
 
     const filenames = data.map(file => file.filename);
     const xml_files = filenames.filter(name => name.endsWith('.xml'));
 
     if (xml_files.length > 0) {
-      console.log(`XML files detected: ${xml_files.join(', ')}`);
+      console.log(`@ XML files detected: ${xml_files.join(', ')}`);
       core.setFailed('XML files are not allowed in this pull request');
     } else {
-      console.log('No XML files detected');
+      console.log('@ No XML files detected');
     }
   } catch (error) {
     core.setFailed(error.message);
