8faae80e0ccab3f1a0892e48f55c8ab40408e367
shimizu_t <shimizu_t@hand.co.jp>
2023-03-24 11:35:53 +0900

    update

--- xml_check.js
+++ xml_check.js
@@ -1,30 +1,42 @@
 const core = require('@actions/core');
-const fetch = require('node-fetch');
+const github = require('@actions/github');
+const fs = require('fs');
 
 async function run() {
   try {
-    const pull_request_url = process.env.GITHUB_PULL_REQUEST_URL;
-    const files_url = `${pull_request_url}/files`;
-    console.log(`@ files_url: ${files_url}`);
-
-    const response = await fetch(files_url);
-    console.log(`@ response: ${response}`);
-
-    const data = await response.json();
+    const token = process.env.GITHUB_TOKEN
+    console.log(`@ token: ${token}`);
+    
+    const pull_number = core.getInput('pull_number');
+    console.log(`@ pull_number: ${pull_number}`);
+    
+    const octokit = github.getOctokit(token);
+    
+    const { data: pullRequest } = await octokit.pulls.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: pull_number
+    });
     console.log(`@ data: ${data}`);
+    
+    const files = pullRequest.changed_files;
 
-    const filenames = data.map(file => file.filename);
-    const xml_files = filenames.filter(name => name.endsWith('.xml'));
+    let contains_xml = false;
+    files.forEach(file => {
+      if (file.endsWith(".xml")) {
+        contains_xml = true;
+        return;
+      }
+    });
 
-    if (xml_files.length > 0) {
-      console.log(`@ XML files detected: ${xml_files.join(', ')}`);
-      core.setFailed('XML files are not allowed in this pull request');
+    if (contains_xml) {
+      console.log("@ This pull request contains at least one XML file.");
     } else {
-      console.log('@ No XML files detected');
+      console.log("@ This pull request does not contain any XML files.");
     }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-module.exports = run;
+run();
