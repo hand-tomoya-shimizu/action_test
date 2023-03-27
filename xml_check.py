78340d9966ca070f4b39d278b48fd952057e6321
shimizu_t <shimizu_t@hand.co.jp>
2023-03-27 14:07:06 +0900

    update

--- xml_check.py
+++ xml_check.py
@@ -3,27 +3,47 @@ import requests
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+
+def add_reviewers():
+    g = Github(os.getenv('GITHUB_TOKEN'))
+    
+    with open('reviewers_test.csv', 'r') as f:
+        reader = csv.reader(f)
+        reviewers = []
+        for row in reader:
+            user = g.get_user(row[1])
+            reviewers.append(user)
+            
+    repo = g.get_repo( os.getenv('GITHUB_REPOSITORY') )
+    pr_number = int(os.getenv('PR_NUMBER'))
+    pr = repo.get_pull(pr_number)
+    
+    pr.create_review_request(reviewers=reviewers)
+
+
 # プルリクエストのURL
 pull_request_url = os.environ['GITHUB_API_URL'] + '/repos/' + os.environ['GITHUB_REPOSITORY'] + '/pulls/' + os.environ['PR_NUMBER']
-print(f"@ pull_request_url: {pull_request_url}")
+#print(f"@ pull_request_url: {pull_request_url}")
 
 # APIリクエストを送信して、変更内容を取得する
 response = requests.get(pull_request_url)
-print(f"@ response1: {response}")
+#print(f"@ response1: {response}")
 
 pull_request_data = response.json()
 
 # 変更されたファイルの内容を取得して、XMLファイルが含まれているか確認する
 diff_url = pull_request_data['diff_url']
-print(f"@ diff_url: {diff_url}")
+#print(f"@ diff_url: {diff_url}")
 
 diff_response = requests.get(diff_url)
-print(f"@ response2: {response}")
+#print(f"@ response2: {response}")
 
 diff_text = diff_response.text
-print(f"@ diff_text: {diff_text}")
+#print(f"@ diff_text: {diff_text}")
 
 if '.xml' in diff_text:
+    add_reviewers()
+    
     print("プルリクエストでXMLファイルが変更されました")
     
     slack_token = os.environ['SLACK_BOT_TOKEN']
@@ -48,3 +68,5 @@ else:
     print("プルリクエストでXMLファイルは変更されていません")
 
 exit(0)
+
+
